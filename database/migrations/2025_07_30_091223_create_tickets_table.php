93891c913c35e299cdf483b579ffa65c4a54b4d9
BramaAntoro <brama4751antoro@gmail.com>
2025-07-30 16:25:10 +0700

    first commit

---
+++ database/migrations/2025_07_30_091223_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['open', 'on_progress', 'resolved', 'rejected'])->default('open');
+            $table->enum('priorty', ['low', 'medium', 'high']);
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
