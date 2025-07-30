a137adeefb2d3eec95f5b2b8d6c159f87de3418c
BramaAntoro <brama4751antoro@gmail.com>
2025-07-30 17:06:46 +0700

    feat: auth login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+
+            if (!Auth::guard('web')->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => "Email or Password failed",
+                    'data' => null
+                ], 401);
+            }
+
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => "Login success",
+                'data' => [
+                    'user' => $user,
+                    'token' => $token
+                ]
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => "Internal server error",
+                'error' => $e->getMessage()
+            ], 500);
+
+        }
+    }
+}
