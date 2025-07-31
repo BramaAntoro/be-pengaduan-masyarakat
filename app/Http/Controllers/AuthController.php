c8964ff1c280f1e8d2e8126f3d6b2434d7349c16
BramaAntoro <brama4751antoro@gmail.com>
2025-07-31 08:27:27 +0700

    feat: register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterStoreRequest;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -75,7 +79,36 @@ class AuthController extends Controller
                 'message' => "Failed logout",
                 'error' => $e->getMessage()
             ], 500);
+        }
+    }
 
+    public function register(RegisterStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        DB::beginTransaction();
+        try {
+
+            $user = User::query()->create($data);
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            DB::commit();
+
+            return response()->json([
+                'message' => "Success Register account",
+                'data' => [
+                    'token' => $token,
+                    'user' => new UserResource($user),
+                ]
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => "Failed Register account",
+                'error' => $e->getMessage()
+            ], 500);
         }
     }
 }
